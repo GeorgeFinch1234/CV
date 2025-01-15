a0aa0f6bf561084297542b39eafad5d7c2ba1392
George Finch <P2654772@my365.dmu.ac.uk>
2025-01-15 11:20:56 +0000

    installed all packages and basic file set up.
    Installed webpack, webpack-cli, babel/core, babel/cli, babel/preset-react, react, react-dom and react-router-dom, babel-loader.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'lib'),
+        filename: 'bundle.js'
+    },
+   
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+                
+            },
+            
+        ],
+       
+        
+    }
+    
+    
+    
+};
