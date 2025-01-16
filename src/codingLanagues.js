880874c6a82459c36c5df0b3b6c4180a5ebf2301
George Finch <P2654772@my365.dmu.ac.uk>
2025-01-16 13:51:53 +0000

    coding langauges pade basic content added

--- src/codingLanagues.js
+++ src/codingLanagues.js
@@ -1,5 +1,37 @@
 export default function Home(){
     return (
+        <>
         <h1> codingLanagaues</h1>
+        <main>
+            <section>
+                <h2>Java</h2>
+                <h3>Java ecosystem</h3>
+                <p>javafx</p>
+                <p>Junit</p>
+                <p>JDoc</p>
+            </section>
+            <section>
+                <h2>JavaScript</h2>
+                <h3>Java ecosystem</h3>
+                <p>node</p>
+                <p>React</p>
+                <p>JSX</p>
+                <p>webpack</p>
+                <p>babel</p>
+                <p>npx</p>
+                <p>react router</p>
+            </section>
+            <section>
+                <h2>other lanvauges</h2>
+                <p>HTML</p>
+                <p>css</p>
+                <p>mySql</p>
+                <h3>very little expriences</h3>
+                <p>C#</p>
+                <p>kotlin</p>
+                <p>scala</p>
+            </section>
+        </main>
+        </>
     )
 }
