ab37babb04163e8aa5500b94bbc0b9664bde1ef3
George Finch <P2654772@my365.dmu.ac.uk>
2025-01-16 13:37:04 +0000

    edcuation page basic content added

--- src/education.js
+++ src/education.js
@@ -1,5 +1,55 @@
-export default function Home(){
+import React from "react"
+export default function Home() {
     return (
-        <h1> eductaion</h1>
+        <>
+            <h1>Eductaion</h1>
+            <main>
+                <article>
+                    <h2>Computer science bsc(Hons)</h2>
+                    <p> De monthfor uni</p>
+                    <p> best second year student</p>
+                    <h3>Modules</h3>
+                    <h4>Mobile application development</h4>
+                    <p> grade 86%</p>
+                    <p>I worked as part of a team using android
+                        studios with Kotlin as the team developed an app for a coffee shop, that would take
+                        orders, edit menu items and allow people to purchase items.</p>
+                    <h4>Development project</h4>
+                    <p> grade 90%</p>
+                    <p>90%. I independently developed a stock management
+                        system using eclipse, java, JavaFX and MySQL. The application had dynamic menus,
+                        CRUD functionality, hashed passwords, it could take info from files and output info to
+                        other files, validation and verification.</p>
+                    <h4>Front end web development</h4>
+                    <p> grade 88%</p>
+                    <p>I developed a rock-climbing website that was
+                        made using HTML, CSS and JavaScript along with coding techniques such as
+                        keyframes, RESTful Apis, JSON and cookies.</p>
+                    <h4>Functional software development</h4>
+                    <p> grade 92%</p>
+                    <p>In this module we used scala following the
+                        functional paradigm in the IntelliJ IDE, as I was taught how to manipulate matrix’s from
+                        one state to another.</p>
+                </article>
+                <article>
+                    <h2>OCR LEVEL 3 CAMBRIDGE TECHNICAL EXTENDED DIPLOMA IN IT,</h2>
+                    <p>NORTHAMPTON COLLEGE</p>
+                    <p>grade Distinction* Distinction* Distinction* </p>
+                </article>
+                <article>
+                    <h2>ALEVELS,,</h2>
+                    <p>NORTHAMPTON COLLEGE</p>
+                    <p>Sociology – C
+                    Geography – D </p>
+                </article>
+                <article>
+                    <h2>GCSE</h2>
+                    <p>Quinton House</p>
+                    <p>7 GCSE – ranging from D – B </p>
+                </article>
+
+            </main>
+        </>
+
     )
 }
