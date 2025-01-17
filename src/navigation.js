32449ddc1015cd687ac36d9f007b86db0ed12cdd
George Finch <P2654772@my365.dmu.ac.uk>
2025-01-17 12:06:28 +0000

    worked on the menu bar for mobile style.

--- src/navigation.js
+++ src/navigation.js
@@ -1,11 +1,19 @@
 import { Outlet, Link } from "react-router-dom";
 
 export default function NavBar() {
+     function menu() {
+        // toggles all the menus for me.
+       
+    
+            document.querySelector(`nav div`).classList.toggle(`show`);
+    
+        
+    }
     return (
         <>
             <nav>
-                <p>{'\u2630'}</p>
-                <div className="menuList show">
+                <p id='menuIcon' onClick={menu}>{'\u2630'}</p>
+                <div className="menuList">
                     <Link to="/lib/index.html" className="link">Home</Link>
                     <Link to="/lib/eductaion" className="link">eductaion</Link>
                     <Link to="/lib/codingLanagaues" className="link">codingLanagaues</Link>
