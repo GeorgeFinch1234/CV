2ff072ea6a1b8f21c4251ba6a50d60560f454441
George Finch <georgefinch12@gmail.com>
2025-06-07 17:37:46 +0100

    small changes.
    Updated webpack so now outputs to docs.
    img now shows.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'lib'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
     },
    
